8aa01cb83315daf1d864501875432b889aff3070
falak-naaz-fatma <176528706+falak-naaz-fatma@users.noreply.github.com.>
2026-04-23 21:13:56 +0530

    proxy file added

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config({ path: ".env.local" });
 
 const nextConfig: NextConfig = {
   /* config options here */
